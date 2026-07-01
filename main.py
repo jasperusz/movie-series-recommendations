52b57c6b021bf173b57c7c91c0fb6bb77305eb9a
jasperusz <235980385+jasperusz@users.noreply.github.com>
2026-07-01 00:04:08 -0300

    feat: created function which return titles from imdb search

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ load_dotenv()
 
 api_key = os.getenv("API_KEY")
 
-response = requests.get(f"http://www.omdbapi.com/?apikey={api_key}&s=Batman")
+def search_movie(search_name):
+    response = requests.get(f"http://www.omdbapi.com/?apikey={api_key}&s={search_name}")
+    returned_data = response.json()
+    for movie in returned_data.get("Search", []):
+        print(f"Title: {movie['Title']}, Year: {movie['Year']}, IMDB ID: {movie['imdbID']}")
 
-print(response.json())
+search_name = input("Enter a movie or series name to search: ")
+
+if __name__ == "__main__":
+    search_movie(search_name)
