dba60d7cf2b43b6e940e9b3e383814116c1bee99
jasperusz <235980385+jasperusz@users.noreply.github.com>
2026-07-01 23:08:38 -0300

    feat: add selection choice and loop while in search menu

--- main.py
+++ main.py
@@ -9,8 +9,20 @@ api_key = os.getenv("API_KEY")
 def search_movie(search_name):
     response = requests.get(f"http://www.omdbapi.com/?apikey={api_key}&s={search_name}")
     returned_data = response.json()
-    for movie in returned_data.get("Search", []):
-        print(f"Title: {movie['Title']}, Year: {movie['Year']}, IMDB ID: {movie['imdbID']}")
+    for i, movie in enumerate(returned_data.get("Search", []), start=1):
+        print(f"{i}. Title: {movie['Title']}, Year: {movie['Year']}, IMDB ID: {movie['imdbID']}")
+    while True:
+        try:
+            choice = int(input("Select the movie or series by selecting the number (or 0 to exit): "))
+            if choice == 0:
+                print("Exiting...")
+                break
+            elif 1 <= choice <= len(returned_data.get("Search", [])):
+                selected_movie = returned_data["Search"][choice - 1]
+                print(f"You selected: {selected_movie['Title']} ({selected_movie['Year']})")
+                break
+        except ValueError:
+            print("Invalid input. Please enter a number corresponding to the movie or series on the list.")
 
 search_name = input("Enter a movie or series name to search: ")
 
